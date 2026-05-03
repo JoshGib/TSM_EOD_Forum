d523ac39db1580522d323b0a1983baf38052c253
Reinald Peguero <reinaldpeguero@gmail.com>
2026-05-03 19:23:03 -0400

    Update dynamic urls

--- ML/EODReportFormat.py
+++ ML/EODReportFormat.py
@@ -9,12 +9,11 @@ df['overall_article_signal'] = df['overall_article_signal'].str.capitalize()
 df['generated_eod_summary'] = df['generated_eod_summary'].str.strip()
 
 df['url_list'] = df['urls'].str.split('|')
-df['first_url'] = df['url_list'].str[0]
 
 df = df.sort_values('trade_day').reset_index(drop=True)
 
-print(df[['trade_day', 'generated_eod_summary', 'overall_article_signal','first_url']].to_string(index=False))
+print(df[['trade_day', 'generated_eod_summary', 'overall_article_signal','url_list']].to_string(index=False))
 
 df.drop(columns=['daily_summary_sentiment','daily_summary_sentiment_score',
-                 'combined_article_summaries','urls','url_list']).to_csv('test_OGTexts_cleaned.csv',
+                 'combined_article_summaries']).to_csv('test_OGTexts_cleaned.csv',
                 index=False, encoding='utf-8')
