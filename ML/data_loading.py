a82733de28871208ff0f5105976c545ab1f338dc
Joshua Gibaja <148903757+JoshGib@users.noreply.github.com>
2026-05-04 17:53:26 -0400

    Refactor data loading script for fine tuning

--- ML/data_loading.py
+++ ML/data_loading.py
@@ -6,14 +6,14 @@
 import pandas as pd
 
 #load data
-Fine_tune_sample = pd.read_csv("fine_tune_sample.csv") #After taking sample went back and edited those that could be salvaged 42 were complete nonsense. used test 
-original_summary_200 = pd.read_csv("train_daily_summaries.csv")
+Fine_tune_sample = pd.read_csv("fine_tune_sample.csv") #After running dor the train data set then idsolated only summaries to then edit by hand.
+original_summary = pd.read_csv("train_daily_summaries.csv")
 
 #keep whats needed only
-original_summary_200 = original_summary_200[["trade_day","combined_article_summaries"]]
+original_summary = original_summary[["trade_day","combined_article_summaries"]]
 
 #merge so only fine tunee sample dates are selected
-merged_df = pd.merge(original_summary_200, Fine_tune_sample, on='trade_day', how='inner')
+merged_df = pd.merge(original_summary, Fine_tune_sample, on='trade_day', how='inner')
 
 #get rid of second index 
 merged_df = merged_df[["trade_day", "combined_article_summaries", "generated_eod_summary"]]
