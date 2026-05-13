0cfeada0cfc0f5e453bf27dd5a626bbb4390b274
Reinald Peguero <reinaldpeguero@gmail.com>
2026-05-12 23:45:31 -0400

    Update page.tsx

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -4,6 +4,6 @@ import { Login } from '../components/Login';
 
 export default function LoginPage() {
   return (<Suspense fallback={<div>Loading...</div>}>
-      <LoginForm />
+      <Login />
     </Suspense>);
 }
